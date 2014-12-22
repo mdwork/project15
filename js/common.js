b42ccbd7dfed0a5ccde803e55235583437fa2b88
mdwork <mazdze@gmail.com>
2014-12-22 17:10:08 +0200

    Zhe1ka

--- js/common.js
+++ js/common.js
@@ -1,3 +1,9 @@
 $(document).ready(function(){
-
+    $('.slider4').bxSlider({
+        slideWidth: 300,
+        minSlides: 2,
+        maxSlides: 3,
+        moveSlides: 1,
+        slideMargin: 10
+    });
 });
