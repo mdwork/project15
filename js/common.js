5b97095d06100fd7bec9fb722f06b4514b68060d
mdwork <mazdze@gmail.com>
2014-12-22 10:34:37 +0200

    Zhe1ka

---
+++ js/common.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+    $('.list-advantage li:nth-child(4n + 2)').css('margin-top', 10 + 'px');
+    $('.list-advantage li:nth-child(4n + 3)').css('margin-top', 20 + 'px');
+    $('.list-advantage li:nth-child(4n + 4)').css('margin-top', 30 + 'px');
+});
