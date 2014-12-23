967a7bfef64887cac7c0f07502f61dbf33ae49b1
mdwork <mazdze@gmail.com>
2014-12-23 10:43:04 +0200

    Zhe1ka

--- js/common.js
+++ js/common.js
@@ -8,4 +8,13 @@ $(document).ready(function(){
     });
 
     $('.list-regulations-work li:nth-child(4n + 1)').addClass('hide-arrow');
+
+    $('.list-choose-parametr label').on('click', function(){
+        var $this = $(this);
+            inputCurrent = $(this).find('input:checked');
+        if(inputCurrent) {
+            $this.parent().siblings().removeClass('active');
+            $this.parent().addClass('active');
+        }
+    });
 });
